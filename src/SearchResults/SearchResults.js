10dc6a9fa3ae377f96a7cc40dda1350747fcb524
anishsharma21 <anisharma2103@gmail.com>
2024-01-05 16:57:47 +1100

    Update CSS styles in Playlist and SearchResults components

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -6,7 +6,6 @@ function SearchResults({ searchResults }) {
   return (
     <div className={styles.SearchResults}>
       <h2 className={styles.SearchResultsHeader}>Search Results</h2> {/* Apply the new class here */}
-      <div className={styles.placeholder}></div> {/* Add this line */}
       <TrackList tracks={searchResults} />
     </div>
   );
